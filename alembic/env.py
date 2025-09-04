e37fc0e52ef1667115c5b32740c2b73ac648660a
kmilo <kmilo.denis.glez@yandex.com>
2025-09-04 19:10:27 -0400

    Add the project root to the path

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the project root to the path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
