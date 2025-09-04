7915f8e7299841b1b7a44867393b5d19d2a9d85e
kmilo <kmilo.denis.glez@yandex.com>
2025-09-04 19:37:06 -0400

    add new attr config

--- app/config.py
+++ app/config.py
@@ -3,10 +3,16 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str
-    test_database_url: str | None = None  # opcional
+    test_database_url: str | None = None
+
+    # JWT / security
+    secret_key: str
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+
 
 settings = Settings()
