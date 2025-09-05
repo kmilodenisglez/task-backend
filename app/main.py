e4132fb1ee0c7edd1d62b292196c57a5ea9f1969
kmilo <kmilo.denis.glez@yandex.com>
2025-09-05 16:44:40 -0400

    clean

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 # app/main.py
 from fastapi import FastAPI
 from app.api.v1 import api_router
-# from app.database import Base, engine
-
-# Create tables (for initial development only, then use Alembic)
-# Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Task API", version="1.0")
 
