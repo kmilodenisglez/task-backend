70a9e11d213e7ccb30dd8da145e621d57d8d2b0e
kmilo <kmilo.denis.glez@yandex.com>
2025-09-07 20:19:20 -0400

    fix

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,7 +2,7 @@
 Utils package - General utility functions and helpers
 """
 
-from .helpers import hash_password, verify_password
+from .auth import hash_password, verify_password
 from .validators import validate_password, validate_email
 
 __all__ = ["hash_password", "verify_password", "validate_password", "validate_email"]
